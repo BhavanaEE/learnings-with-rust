e604d6805772acac8e31c0df6d1df060f8fdd3a6
Bhavana Chivukula <bhavana.chivukula@everest.engineering>
2022-12-29 13:36:52 +0530

    panic on error - unwrap, expect

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,13 @@
 // Error handling
+// shortcut for panic using unwrap and expect
+use std::fs::File;
 
-use std::{fs::File, error::Error};
-use std::io::ErrorKind;
 fn main() {
-    let mut f = File::open("hello.txt");
 
-    // shadowing to redeclare f
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file {:?}",e),
-            }
+    //unwrap
+    let f1 = File::open("hello.txt").unwrap();
 
-            other_error => {
-                panic!("problem opening the file {:?}",other_error)
-            }
-        }
-    };
-
-
+    // expect
+    let f2 = File::open("hello.txt")
+        .expect("No file named hello.txt");
 }
