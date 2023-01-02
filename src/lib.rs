2f132454dc72bac4c392643e3e3e3865f6fadcf6
Bhavana Chivukula <bhavana.chivukula@everest.engineering>
2023-01-02 08:51:22 +0530

    Testing - assert with custom message

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,16 @@
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
+pub fn greeting(name: &str) -> String {
+    format!("Hello! {}", name)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Rectangle;
+    use super::*;
 
     #[test]
-    fn large_value_can_hold_small_value() {
-        let large_rect = Rectangle { width: 6, height: 7};
-        let small_rect = Rectangle { width: 5, height: 6};
+    fn greeting_contains_name() {
+        let greet = greeting("Bhavana");
         
-        assert!(large_rect.can_hold(&small_rect)); 
+        // assert with custom message
+        assert!(greet.contains("na"),"Greeting doesn't contain name, value was {}",greet); 
     }
 }
