eafed4644f94e1cac7172cf368596074726d42f2
Bhavana Chivukula <bhavana.chivukula@everest.engineering>
2023-01-04 10:49:35 +0530

    day-2 task: Creation of AP series

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,34 @@
-// Traits
-
-pub trait Summary {
-    fn summarize(&self) -> String {
-        String::from("Default implementation..")
-    }
-
-    fn summarize_author(&self) -> String;
+struct AP {
+    curr: u32,
+    common_diff: u32,
 }
 
-// trait bound
-pub fn notify_studemt_exam_marks<T,U>(item1:T,item2:U) -> String
-    where T: Summary,
-          U: Summary
-{
-    format!("exam marks notificaation for student name: {} and {}",item1.summarize_author(),item2.summarize())
+impl Iterator for AP {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current = self.curr;
+
+        self.curr = current + self.common_diff;
+        Some(current)
+    }
 }
 
-pub struct Person {
-    pub name: String,
-    pub age: u32,
-    pub gender: char,
-}
-
-impl Summary for Person {
-    fn summarize_author(&self) -> String {
-        format!("@ author - {}",self.name)
-    }
-} 
-
-pub struct Student {
-    pub roll_number: u32,
-    pub name: String,
-    pub class: u32,
-    pub marks_sheet: u32
-}
-
-impl Summary for Student {
-    fn summarize(&self) -> String {
-        format!("Student name is:{} and roll number is:{}",self.name,self.roll_number)
-    }
-
-    fn summarize_author(&self) -> String {
-        format!("@ author {}",self.name)
-    }
+fn arithmetic_progression() -> AP {
+    AP { curr: 1, common_diff: 3 }
 }
 
 fn main() {
-    let per = Person {
-        name: String::from("Bhavana"),
-        age:23,
-        gender: 'F',
-    };
 
-    let stu = Student {
-        roll_number: 66,
-        name: String::from("Bhavana"),
-        class: 1,
-        marks_sheet: 95,
-    };
+    println!("The first four terms of the Arithmetic progression sequence are: ");
+    for i in arithmetic_progression().take(4) {
+        print!("{} ", i);
+    }
 
-    println!("Person details: {} {}",per.summarize_author(),per.summarize()); // Person details = Default implementation..
-    println!("Student details: {} {}",per.summarize_author(),stu.summarize()); // Student details = Student name is:Bhavana and roll number is:66
+    println!("\nThe next four terms of the Arithmetic progression sequence are: ");
+    for i in arithmetic_progression().skip(4).take(4) {
+        print!("{} ", i);
+    }
 
-    // trait bound
-    println!("{}",notify_studemt_exam_marks(per, stu));
 }
+
